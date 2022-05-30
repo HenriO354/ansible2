8291257ad9f12ab4fc68df9cfd6dd87d426d8dab
Michel Difils <michel.fils@hotmail.com>
2022-05-30 14:43:17 +0000

    Updates

--- hill/vagrant/bootstrap.sh
+++ hill/vagrant/bootstrap.sh
@@ -6,31 +6,25 @@ DBUSER=cogip
 DBPASSWD=cogippass
 
 # Update Packages
-apt update
+apt update > /dev/null 2>&1
 # Upgrade Packages
-apt upgrade -y
+apt upgrade -y > /dev/null 2>&1
 
 # Apache
-apt install -y apache2
+apt install -y apache2 > /dev/null 2>&1
 
 # Enable Apache Mods
 a2enmod rewrite
 
-# Install PHP
-apt install -y php
-
-# PHP Apache Mod
-apt install -y libapache2-mod-php
-
-# Restart Apache
-systemctl restart apache2
+# restart apache
+service apache2 restart
 
 # Set MySQL Pass
 debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
 debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
 
 # Install MySQL
-apt install -y mysql-server
+apt install -y mysql-server > /dev/null 2>&1
 
 # Create cogip database
 mysql -uroot -proot -e "CREATE DATABASE $DBNAME;"
@@ -44,11 +38,11 @@ mysql -uroot -proot $DBNAME < /var/www/html/database/cogip.sql
 # Insert a user into user table
 mysql -uroot -proot $DBNAME -e "INSERT INTO user(username,password,mode) VALUES('Michel','michelpassword','winner');"
 
-# PHP-MYSQL lib
-apt install -y php-mysql
+# PHP, PHP Apache Mod and PHP-MYSQL lib
+apt install -y php libapache2-mod-php php-mysql > /dev/null 2>&1
 
 # Edit apache default config
-mv /var/wwww/html/000-default.conf /etc/apache2/sites-available/000-default.conf
+cp /var/www/html/config/000-default.conf /etc/apache2/sites-available/000-default.conf
 
 # Restart Apache
-systemctl restart apache2
+service apache2 restart
